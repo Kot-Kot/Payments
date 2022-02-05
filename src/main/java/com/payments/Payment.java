d640dd09b3435a7cab318a135f942ea25d7f4cee
Kot-Kot <ukrgipromez.it@gmail.com>
2022-02-05 17:39:59 +0200

    commit 220205

--- src/main/java/com/payments/Payment.java
+++ src/main/java/com/payments/Payment.java
@@ -1,6 +1,7 @@
 package com.payments;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Payment {
     private int id;
@@ -15,4 +16,22 @@ public class Payment {
     private LocalDateTime creationDateTime;
     private LocalDateTime statusChangedDateTime;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return id == payment.id &&
+                Double.compare(payment.sum, sum) == 0 &&
+                Objects.equals(template, payment.template) &&
+                Objects.equals(cardNumber, payment.cardNumber) &&
+                Objects.equals(creationDateTime, payment.creationDateTime) &&
+                Objects.equals(statusChangedDateTime, payment.statusChangedDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, template, cardNumber, sum, creationDateTime, statusChangedDateTime);
+    }
 }
